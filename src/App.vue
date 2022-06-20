c1255972981f9e4c662173472d7c63d6c562dadd
Sille Van Landschoot <info@sillevl.be>
2022-06-20 09:50:29 +0200

    read serial port

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <button @click="connect">Connect</button>
+  <label for="console">
+    <textarea name="console" id="console" v-model="log" cols="30" rows="10"></textarea>
+  </label>
 </template>
 
 <script lang="ts">
+
+/* eslint-disable no-await-in-loop */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import { Options, Vue } from 'vue-class-component';
 import HelloWorld from './components/HelloWorld.vue';
 
@@ -12,7 +18,75 @@ import HelloWorld from './components/HelloWorld.vue';
     HelloWorld,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  tmp = 10;
+
+  port: unknown = undefined;
+
+  log = '';
+
+  buffer = '';
+
+  static mounted() {
+    if ('serial' in navigator) {
+      console.log('Awesome, The serial port is supported.');
+      // The Web Serial API is supported.
+    }
+  }
+
+  async connect() {
+    console.log(`connect button pressed ${this.tmp}`);
+    navigator.serial
+      .requestPort({ filters: [] })
+      .then(async (port) => {
+        // Initialize the list of available ports with `ports` on page load.
+        this.port = port;
+        console.log('saving port');
+        await port.open({ baudRate: 115200 });
+
+        while (port.readable) {
+          const reader = port.readable.getReader();
+          try {
+            // eslint-disable-next-line no-constant-condition
+            while (true) {
+              const { value, done } = await reader.read();
+              if (done) {
+                // |reader| has been canceled.
+                break;
+              }
+              // Do something with |value|...
+              const chunk :string = new TextDecoder().decode(value);
+              this.buffer += chunk;
+              const position = this.buffer.indexOf('\n');
+              if (position !== -1) {
+                const line = this.buffer.substring(0, position).replaceAll('\r', '');
+                const parts = line.split(',');
+                const data = {
+                  raw: parts[0],
+                  standard_algorithm: parts[1],
+                  confidence: {
+                    push: parts[2],
+                    release: parts[3],
+                    idle: parts[4],
+                  },
+                };
+                console.log(data);
+                this.log += line;
+              }
+            }
+          } catch (error) {
+            // Handle |error|...
+          } finally {
+            reader.releaseLock();
+          }
+        }
+      })
+      .catch((e: Error) => {
+        // The user didn't select a port.
+        console.log(e);
+      });
+  }
+}
 </script>
 
 <style>
